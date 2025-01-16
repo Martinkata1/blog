d1c36a9046c806e27e1ac129c817e654c376baab
Martinkata1 <marti.kostadinov2003@gmail.com>
2025-01-16 17:32:34 +0200

    Added some things and wave colorful.

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ function toggleSidebar() {
     sidebar.classList.toggle('hidden');
     content.classList.toggle('shifted');
 }
+
+
 const menuBtn = document.getElementById("menu-btn");
 const sidebar = document.getElementById("sidebar");
 
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("active");
 });
-function showSection(sectionId) {
-    document.querySelectorAll('.section').forEach(section => {
-        section.style.display = 'none';
-    });
-    document.getElementById(sectionId).style.display = 'block';
-}
