cffa07f8d8d45f1eec5d3e21a3e09fa035e3c134
Martinkata1 <marti.kostadinov2003@gmail.com>
2025-01-17 10:32:24 +0200

    Upgraded collection.html

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-console.log('Happy developing ✨');
 document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.add('hidden');
 });
+//function toggleSidebar() {
+//     const sidebar = document.querySelector('.sidebar');
+//     const content = document.querySelector('.content');
+//     sidebar.classList.toggle('hidden');
+//     content.classList.toggle('shifted');
+// }
 function toggleSidebar() {
-    const sidebar = document.querySelector('.sidebar');
-    const content = document.querySelector('.content');
-    sidebar.classList.toggle('hidden');
-    content.classList.toggle('shifted');
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('hidden');  // Превключване на състоянието на менюто
 }
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animContainer'),
+    path: 'https://lottie.host/a38c653d-74fb-4325-802a-4737837e11de/smLG7WMgmu.lottie',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true
+});
